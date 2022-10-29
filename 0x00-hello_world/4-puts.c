acf0ad29c0150b2db12b23afe64e1dafd2d034ff
HISmercytech <Ayokujore@gmail.com>
2022-10-29 13:26:24 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/* c program that prints string of character with a new line
- *  *main- Serves as the entry point for the program
- *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
+/*c program that prints string of character with a new line
+ *main- Serves as the entry point for the program
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return(0);
 }
