2304e829764a55a40c58adf855254b9e4c11f6ac
HISmercytech <Ayokujore@gmail.com>
2022-10-29 12:29:48 -0700

    task1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* c program that prints string of character with a new line */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
